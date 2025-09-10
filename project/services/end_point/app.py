44c573c61be31e52f99c6c161ea50a8a6c3bbd62
chagay <chagaytoubin@gmail.com>
2025-09-10 12:59:48 +0300

    containers

--- project/services/end_point/app.py
+++ project/services/end_point/app.py
@@ -2,31 +2,24 @@ from fastapi import FastAPI
 from project.config.settings import Settings
 import os
 from project.utilities.elastic.elastic import Elastic
-#
-# def get_uri(host=Settings.uri_es[1],port=Settings[0]):
-#     if host and port:
-#         return f"http://{host}:{port}"
-#     else:
-#         return "http://localhost:9200"
+from project.utilities.logger.logger_info import Logger
+
+logger=Logger.get_logger()
+
 
 
 app = FastAPI()
 
 es =Elastic(Settings.uri_es)
-
-
-
+logger.info("The server work")
 
 @app.get("/high_risk")
 def hige_risk():
     return es.find_risk("high")
 
-
 @app.get("/medium_risk")
 def medium_risk():
     return es.find_risk("medium")
-
-
 
 @app.get("/none_risk")
 def medium_risk():
